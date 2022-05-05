fede06680ff57fc9990a09a6ba2f93f2d2f5d4fb
Jessicaawoh <jessicaawoh@gmail.com>
2022-05-05 11:57:50 -0400

    Update streamlit_app.py
    added smoothie favorites

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.text('🥣Omega 3 & blueberry oatmeal')
 streamlit.text('🥗Kale, Spinach, and Rocket Smoothie')
 streamlit.text( '🐔Hard-boiled Free ranged egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
