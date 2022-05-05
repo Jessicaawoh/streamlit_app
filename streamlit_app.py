72b5c63c0da877d902a192b43f6a2d55446ed2f6
Jessicaawoh <jessicaawoh@gmail.com>
2022-05-05 11:52:39 -0400

    Update streamlit_app.py
    added emojis and changed the breakfast headline

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale, Spinach, and Rocket Smoothie')
-streamlit.text('Hard-boiled Free ranged egg')
-
+streamlit.header('Breakfast Favorites')
+streamlit.text(:tea:'Omega 3 & blueberry oatmeal')
+streamlit.text(:stew: 'Kale, Spinach, and Rocket Smoothie')
+streamlit.text(:egg: 'Hard-boiled Free ranged egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
