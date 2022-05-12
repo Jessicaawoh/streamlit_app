f58ae8091bcff659778a5b557a3f94d16fbaa824
Jessicaawoh <jessicaawoh@gmail.com>
2022-05-12 13:03:15 -0400

    Update streamlit_app.py
    removes the array string line for the response and request

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,6 @@ streamlit.header('fruityvice Fruit Advice!')
 
 import requests 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #writes the data to the screen 
 
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
